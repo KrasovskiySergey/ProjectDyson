3fe637407778052df4e16aca8adba34a91fb92b6
TheOneGuy <157389256+KrasovskiySergey@users.noreply.github.com>
2025-06-09 22:19:11 +0300

    +

---
+++ js/burger.js
@@ -0,0 +1,23 @@
+(function() {
+    // Бургер-----------------------------------
+
+    document.addEventListener('click', burgerInit)
+
+    function burgerInit(e) {
+
+        const burgerIcon = e.target.closest('.burger')
+        const burgerNavLink = e.target.closest('.nav__link')
+
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 1450) return
+
+        
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+    }
+
+})()
